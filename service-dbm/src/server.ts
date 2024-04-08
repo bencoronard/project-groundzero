f60e8ee9ed4f1fc90b087f2c9f07f1d4f8d27e8e
Benjamin <128577990+bencoronard@users.noreply.github.com>
2024-04-08 23:54:52 +0700

    Updated server.ts #6

--- service-dbm/src/server.ts
+++ service-dbm/src/server.ts
@@ -4,66 +4,103 @@ import { ExpressHTTP } from './detachables/ExpressHTTP';
 import { Interactor } from './operators/Interactor';
 import { Controller } from './operators/Controller';
 import { RecordRepository } from './entities/RecordRepository';
-// import { StorageMySQL } from './detachables/StorageMySQL';
+import { StorageMySQL } from './detachables/StorageMySQL';
 import { StorageMongoDB } from './detachables/StorageMongoDB';
 
-dotenv.config();
-const PORT: number = parseInt(process.env.PORT || '3000', 10);
-
-const config = JSON.parse(process.env.CONFIG_MYSQL);
-const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'valorant',
-  table: 'agents',
-};
-
-const config = JSON.parse(process.env.CONFIG_MONGO);
-const dbConfig = {
-  uri: 'mongodb://localhost:27017',
-  database: 'valorant',
-  collection: 'agents',
-};
 const app = express();
-
 main();
 
 async function main(): Promise<void> {
   try {
-    const db: RecordRepository = await initialize();
+    const { port: PORT, db: dbType, config: dbConfig } = await setup();
+    const database: RecordRepository = await initialize(dbType, dbConfig);
+
     process.on('SIGINT', async () => {
-      await closeDB(db);
+      await closeDB(database);
     });
+
     app.listen(PORT, () => {
       console.log(`Server listening on port: ${PORT}`);
     });
-  } catch {
-    console.error('Error starting the server');
+  } catch (error) {
+    console.error(`Error starting the server...\n`, error);
     process.exit(1);
   }
 }
 
-async function initialize(): Promise<RecordRepository> {
-  // const db = new StorageMySQL(dbConfig);
-  const db = new StorageMongoDB(dbConfig);
-  const interactor = new Interactor(db);
-  const controller = new Controller(interactor);
-
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.all('/records*', async (req, res) => {
-    try {
-      const reqHTTP = new ExpressHTTP(req);
-      const resHTTP = await controller.route(reqHTTP);
-      res.status(resHTTP.statusCode);
-      res.set(resHTTP.headers);
-      res.send(resHTTP.body);
-    } catch (error) {
-      res.status(500).send((error as Error).message);
+async function setup(): Promise<{
+  port: number;
+  db: string;
+  config: { [key: string]: string };
+}> {
+  try {
+    dotenv.config();
+    const result = { port: 0, db: '', config: {} };
+    if (process.env.PORT && process.env.DB_TYPE) {
+      result.port = parseInt(process.env.PORT, 10);
+      const dbType: string = process.env.DB_TYPE;
+      if (process.env.CONFIG_MYSQL && dbType === 'MySQL') {
+        const config = JSON.parse(process.env.CONFIG_MYSQL);
+        if (
+          config.host &&
+          config.user &&
+          config.password &&
+          config.database &&
+          config.table
+        ) {
+          result.db = dbType;
+          result.config = config;
+        } else {
+          throw new Error('Incomplete database config');
+        }
+      } else if (process.env.CONFIG_MONGO && dbType === 'MongoDB') {
+        const config = JSON.parse(process.env.CONFIG_MONGO);
+        if (config.uri && config.database && config.collection) {
+          result.db = dbType;
+          result.config = config;
+        } else {
+          throw new Error('Incomplete database config');
+        }
+      } else {
+        throw new Error('Missing or invalid database config');
+      }
+      return result;
+    } else {
+      throw new Error('Missing environment variables');
     }
-  });
-  return db;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function initialize(
+  dbType: string,
+  dbConfig: { [key: string]: string }
+): Promise<RecordRepository> {
+  try {
+    const database = new StorageMySQL(dbConfig);
+    const database = new StorageMongoDB(dbConfig);
+
+    const interactor = new Interactor(database);
+    const controller = new Controller(interactor);
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.all('/records*', async (req, res) => {
+      try {
+        const reqHTTP = new ExpressHTTP(req);
+        const resHTTP = await controller.route(reqHTTP);
+        res.status(resHTTP.statusCode);
+        res.set(resHTTP.headers);
+        res.send(resHTTP.body);
+      } catch (error) {
+        res.status(500).send((error as Error).message);
+      }
+    });
+    return database;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function closeDB(db: RecordRepository): Promise<void> {
@@ -72,7 +109,7 @@ async function closeDB(db: RecordRepository): Promise<void> {
     console.log('Database connections closed');
     process.exit(0);
   } catch {
-    console.log('Error closing database connections');
+    console.error('Error closing database connections');
     process.exit(1);
   }
 }
