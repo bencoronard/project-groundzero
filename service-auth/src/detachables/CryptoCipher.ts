4f7634820dd82664431d97dcf492af5dbe604e68
Benjamin <128577990+bencoronard@users.noreply.github.com>
2024-05-01 10:58:28 +0700

    Updated CryptoCipher.ts

--- service-auth/src/detachables/CryptoCipher.ts
+++ service-auth/src/detachables/CryptoCipher.ts
@@ -2,7 +2,21 @@ import crypto from 'crypto';
 import { Cipher } from '../entities/Cipher';
 
 export class CryptoCipher implements Cipher {
-  constructor() {}
+  private binEncoding: BufferEncoding;
+  private charEncoding: BufferEncoding;
+  private symmAlgorithm: string;
+  private sep: string;
+
+  constructor() {
+    // Default binary encoding
+    this.binEncoding = 'base64';
+    // Default character encoding
+    this.charEncoding = 'utf-8';
+    // Default symmetric encrypting algorithm
+    this.symmAlgorithm = 'aes-256-cbc';
+    // Default separator for symmetric encryption (IV + sep + encryptedData)
+    this.sep = ':';
+  }
 
   async generateKeyPair(): Promise<{ publicKey: string; privateKey: string }> {
     try {
@@ -20,33 +34,95 @@ export class CryptoCipher implements Cipher {
     }
   }
 
-  async encryptText(plainText: string, key: string): Promise<string> {
+  async encryptSymmetric(plainText: string, key: string): Promise<string> {
     try {
-      // Create binary string from input text
-      const buffer = Buffer.from(plainText, 'utf-8');
-      // Encrypt binary string
-      const encrypted = crypto.publicEncrypt(key, buffer);
-      // const encrypted = crypto.privateEncrypt(key, buffer);
+      // Generate a random Initialization Vector (128bit)
+      const iv = crypto.randomBytes(16);
+      // Create AES cipher with key and IV
+      const cipher = crypto.createCipheriv(
+        this.symmAlgorithm,
+        Buffer.from(key, this.binEncoding),
+        iv
+      );
+      // Initialize encryption task
+      let encrypted = cipher.update(
+        plainText,
+        this.charEncoding,
+        this.binEncoding
+      );
+      // Encrypt chunks of data and append to result
+      encrypted += cipher.final(this.binEncoding);
+      // Combine IV and encrypted data
+      const combined = iv.toString(this.binEncoding) + this.sep + encrypted;
+      // Return symmetrically encrypted text
+      return combined;
+    } catch (error) {
+      // Text encryption failed
+      throw new Error('Module unable to symmetrically encrypt input');
+    }
+  }
 
-      // Return encrypted binary string as text
-      return encrypted.toString('base64');
-    } catch {
+  async decryptSymmetric(cipherText: string, key: string): Promise<string> {
+    try {
+      // Split IV and encrypted data
+      const parts = cipherText.split(this.sep);
+      // Extract IV from encryption
+      const iv = Buffer.from(parts[0], this.binEncoding);
+      // Extract encrypted data
+      const encryptedData = parts[1];
+      // Create AES decipher with key and extracted IV
+      const decipher = crypto.createDecipheriv(
+        this.symmAlgorithm,
+        Buffer.from(key),
+        iv
+      );
+      // Initialize decryption task
+      let decrypted = decipher.update(
+        encryptedData,
+        this.binEncoding,
+        this.charEncoding
+      );
+      // Decrypt chunks of data and append to result
+      decrypted += decipher.final(this.charEncoding);
+      // Return decrypted symmetrically encrypted text
+      return decrypted;
+    } catch (error) {
+      // Text decryption failed
+      throw new Error('Module unable to symmetrically decrypt input');
+    }
+  }
+
+  async encryptAsymmetric(
+    plainText: string,
+    publicKey: string
+  ): Promise<string> {
+    try {
+      // Encrypt the input text using RSA public key
+      const encrypted = crypto.publicEncrypt(
+        publicKey,
+        Buffer.from(plainText, this.charEncoding)
+      );
+      // Return encrypted binary string as base64 encoded text
+      return encrypted.toString(this.binEncoding);
+    } catch (error) {
       // Text encryption failed
       throw new Error('Module unable to encrypt input');
     }
   }
 
-  async decryptText(cipherText: string, key: string): Promise<string> {
+  async decryptAsymmetric(
+    cipherText: string,
+    privateKey: string
+  ): Promise<string> {
     try {
-      // Create binary string from input text
-      const buffer = Buffer.from(cipherText, 'base64');
-      // Decrypt binary string
-      const decrypted = crypto.privateDecrypt(key, buffer);
-      // const decrypted = crypto.publicDecrypt(key, buffer);
-
-      // Return decrypted binary string as text
-      return decrypted.toString('utf-8');
-    } catch {
+      // Decrypt the encrypted data using RSA private key
+      const decrypted = crypto.privateDecrypt(
+        privateKey,
+        Buffer.from(cipherText, this.binEncoding)
+      );
+      // Return decrypted binary string as UTF-8 encoded text
+      return decrypted.toString(this.charEncoding);
+    } catch (error) {
       // Text decryption failed
       throw new Error('Module unable to decrypt input');
     }
--- service-auth/src/entities/Cipher.ts
+++ service-auth/src/entities/Cipher.ts
@@ -1,5 +1,7 @@
 export interface Cipher {
   generateKeyPair(): Promise<{ publicKey: string; privateKey: string }>;
-  encryptText(plainText: string, key: string): Promise<string>;
-  decryptText(cipherText: string, key: string): Promise<string>;
+  encryptSymmetric(plainText: string, key: string): Promise<string>;
+  decryptSymmetric(cipherText: string, key: string): Promise<string>;
+  encryptAsymmetric(plainText: string, publicKey: string): Promise<string>;
+  decryptAsymmetric(cipherText: string, privateKey: string): Promise<string>;
 }
